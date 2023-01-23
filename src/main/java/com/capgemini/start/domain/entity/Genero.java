6dc29b098e9104aae9b30530a44ddfa6f28a49c6
renanrcs <renan.cavalcante@yahoo.com>
2023-01-23 16:46:13 -0300

    class Genero created

---
+++ src/main/java/com/capgemini/start/domain/entity/Genero.java
@@ -0,0 +1,34 @@
+package com.capgemini.start.domain.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true) //usar apenas que esta com Include
+@Entity(name = "tb_genero")
+public class Genero {
+	
+	@EqualsAndHashCode.Include //especifica que para o hash
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_genero", nullable = false)
+	private Integer id;
+	
+	@Column(name = "ds_genero", nullable = false, length = 100)
+	private String descricao;
+	
+	@Column(name = "dt_inclusao", nullable = false)
+	private Date dataInclusao;
+	
+	@Column(name = "dt_alteracao")
+	private Date dataAlteracao;
+
+}

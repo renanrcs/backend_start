1434b5f1fcd899f0b5986ec41577933dee269aca
renanrcs <renan.cavalcante@yahoo.com>
2023-01-23 20:17:32 -0300

    class GeneroDTO ceated

---
+++ src/main/java/com/capgemini/start/api/dto/output/GeneroDTO.java
@@ -0,0 +1,16 @@
+package com.capgemini.start.api.dto.output;
+
+import java.time.LocalDateTime;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+@Schema(name = "Genero", description = "Objeto de saida da entidade Genero")
+@Data
+public class GeneroDTO {
+	
+	private Integer id;
+	private String descricao;
+	private LocalDateTime dataCriacao;
+	private LocalDateTime dataAlteracao;
+
+}

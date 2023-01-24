a4085f1e86811a4db7fc8897e29ed4bb4cf2f62d
renanrcs <renan.cavalcante@yahoo.com>
2023-01-24 09:03:07 -0300

    add generos in DB

--- src/main/java/com/capgemini/start/util/LoadData.java
+++ src/main/java/com/capgemini/start/util/LoadData.java
@@ -8,7 +8,9 @@ import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.capgemini.start.domain.entity.Genero;
 import com.capgemini.start.domain.entity.Tipo;
+import com.capgemini.start.domain.repository.GeneroRepository;
 import com.capgemini.start.domain.repository.TipoRepository;
 
 @Component
@@ -17,15 +19,25 @@ public class LoadData {
 	@Autowired
 	private TipoRepository tipoRepository;
 	
+	@Autowired
+	private GeneroRepository generoRepository;
+	
 	private void carregarTipos() {
 		var filmes = new Tipo(null, "Filmes", new Date(), null);
 		var series = new Tipo(null, "Series", new Date(), null);
 		tipoRepository.saveAll(List.of(filmes, series));
 	}
 	
+	private void carregarGeneros() {
+		var genAcao = new Genero(null, "Ação", new Date(), null);
+		var genComedia = new Genero(null, "Comédia", new Date(), null);
+		generoRepository.saveAll(List.of(genAcao, genComedia));
+	}
+	
 	@PostConstruct
 	public void carregar() {
 		carregarTipos();
+		carregarGeneros();
 	}
 
 }

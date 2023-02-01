ccb79fdd4bfaccc8ff2e5a3567b8fe4d1104205d
renanrcs <renan.cavalcante@yahoo.com>
2023-02-01 19:42:58 -0300

    update import

--- src/main/java/com/capgemini/start/api/resource/impl/GeneroResourceImpl.java
+++ src/main/java/com/capgemini/start/api/resource/impl/GeneroResourceImpl.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -21,7 +22,6 @@ import com.capgemini.start.api.resource.GeneroResource;
 import com.capgemini.start.domain.entity.Genero;
 import com.capgemini.start.domain.service.GeneroService;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
